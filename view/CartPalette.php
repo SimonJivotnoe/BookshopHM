41cff3eab202090599dcf3b89645001252a106ff
SimonJivotnoe <nk271085mam@gmail.com>
2015-04-10 10:48:29 +0300

    Update CartPalette.php

--- view/CartPalette.php
+++ view/CartPalette.php
@@ -1,14 +1,24 @@
 <?php
 
 
+/**
+ * Class CartPalette
+ */
 class CartPalette
 {
     private $repArr = array();
+
+    /**
+     *
+     */
     public function __construct()
     {
 
     }
 
+    /**
+     * @return array
+     */
     public function getArr()
     {
         return $this->repArr;
