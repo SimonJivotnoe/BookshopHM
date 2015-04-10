c68278d1eeb8dad66bcf6fcd11310ac192ac2264
SimonJivotnoe <nk271085mam@gmail.com>
2015-04-10 10:47:32 +0300

    Update SubstitutionModel.php

--- view/SubstitutionModel.php
+++ view/SubstitutionModel.php
@@ -1,12 +1,23 @@
 <?php
 
 
+/**
+ * Class SubstitutionModel
+ */
 class SubstitutionModel {
 
+    /**
+     *
+     */
     public function __construct() {
 
     }
 
+    /**
+     * @param $page
+     * @param $lang
+     * @return string
+     */
     public function parseAndReplace($page, $lang){
         $htmlToString = file_get_contents(TEMPLATES . $page[0]);
             $palette = $this->getPaletteName($page[0]).'Palette';
@@ -14,17 +25,38 @@ class SubstitutionModel {
             $palette = ucfirst($palette);						
             $objPalette = new $palette();
             $result = strtr($htmlToString, $objPalette->getArr());
-            //var_dump($result);
             $resultLang = strtr($result, $lang);
             return $resultLang;
     }
 
+    /**
+     * @param $page
+     * @return mixed
+     */
     private function getPaletteName($page){
         return str_replace('.html', '',$page);
     }
 
+    /**
+     * @param $page
+     * @param $arr
+     * @return string
+     */
     public function langReplace($page, $arr){
         $result = strtr($page, $arr);
         return $result;
     }
+
+    /**
+     * @param $page
+     * @param $placeholders
+     * @return string
+     */
+    public function subHTMLReplace($page, $placeholders){
+        $htmlToString = file_get_contents(SUBTEMPLATES . $page);
+        $result = strtr($htmlToString, $placeholders);
+        return $result;
+    }
 } 
+
+
