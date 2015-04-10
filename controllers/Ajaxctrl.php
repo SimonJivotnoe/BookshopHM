53263ebf4d21c64185678ba370989c4a1b9aa4b0
SimonJivotnoe <nk271085mam@gmail.com>
2015-04-10 10:41:34 +0300

    Update Ajaxctrl.php

--- controllers/Ajaxctrl.php
+++ controllers/Ajaxctrl.php
@@ -1,9 +1,25 @@
 <?php
+/*
 class Ajaxctrl {
 
     public function __construct(){
         $pdo = DataBaseModel::connect();
         $result= $pdo->select('* ')->from($_GET['table'])->exec();
+        DataContModel::getInstance()->setData($result);
+    }
+
+}*/
+/**
+ * Class AjaxCtrl
+ */
+class AjaxCtrl {
+
+    /**
+     *
+     */
+    public function __construct(){
+        $objModel = new AgentDBModel();
+        $result = $objModel->selectAll($_GET['table']);
         DataContModel::getInstance()->setData($result);
     }
 
