4bc27ee977555576e439cb7563ba40a3df388b27
SimonJivotnoe <nk271085mam@gmail.com>
2015-04-10 10:49:00 +0300

    Update ConfirmorderPalette.php

--- view/ConfirmorderPalette.php
+++ view/ConfirmorderPalette.php
@@ -1,6 +1,6 @@
 <?php
 
-
+/*
 class ConfirmorderPalette {
     private $quantity = 0;
     private $price = 0;
@@ -32,6 +32,65 @@ class ConfirmorderPalette {
         }
         foreach ($arr['payment'] as $key => $val){
             $this->payment .= "<input type='radio' name='payment' value='".$val['payment_id']."'required>".$val['payment_name'];
+        }
+        if ($this->discount > 0) {
+            $this->price = $this->price - $this->price / $this->discount;
+        }
+        $this->repArr['%QUANTITY%']=$this->quantity;
+        $this->repArr['%PRICE%']=$this->price;
+        $this->repArr['%PAYMENT%']=$this->payment;
+        $this->repArr['%DISCOUNT%']=$this->discount;
+        $this->repArr['%FINALPRICE%']=$this->price;
+        return $this->repArr;
+    }
+} 
+*/
+
+/**
+ * Class ConfirmorderPalette
+ */
+class ConfirmorderPalette {
+    private $quantity = 0;
+    private $price = 0;
+    private $discount = 0;
+    private $payment = '';
+    private $repArr = array(
+    '%QUANTITY%'  => '',  
+    '%PRICE%' => '',
+    '%PAYMENT%' => '',        
+    '%DISCOUNT%' => '',        
+    '%FINALPRICE%' => '',        
+    );
+
+    /**
+     *
+     */
+    public function __construct()
+    {
+
+    }
+
+    /**
+     * @return array
+     */
+    public function getArr()
+    {
+        $obj = DataContModel::getInstance();
+        $arr = $obj->getData();
+        $sub = new SubstitutionModel();
+        foreach ($arr['quantityAndPrice'] as $key => $val){
+            $this->quantity += (int)$val['quantity'];
+            $this->price += (int)$val['quantity'] * (int)$val['price'];
+        }
+        foreach ($arr['discount'] as $key => $val){
+            $this->discount += (int)$val['user_discount'];
+
+        }
+        foreach ($arr['payment'] as $key => $val){
+            //$this->payment .= "<input type='radio' name='payment' value='".$val['payment_id']."'required>".$val['payment_name'];
+            $this->payment .= $sub->subHTMLReplace('typeOfPayment.html',
+                array('%PAYMENT_ID%' => $val['payment_id'],
+                      '%PAYMENT_NAME%' => $val['payment_name'],));
         }
         if ($this->discount > 0) {
             $this->price = $this->price - $this->price / $this->discount;
