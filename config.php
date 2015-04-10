84eb05b44fa4861bd51cf66152be5e44554717d3
SimonJivotnoe <nk271085mam@gmail.com>
2015-04-10 10:46:52 +0300

    Update config.php

--- config.php
+++ config.php
@@ -5,6 +5,7 @@ define ('PASS', 'tuser1');
 define ('DB', 'user1');
 define ('HOME_TEMPLATE', 'templates/html/index.html');
 define ('TEMPLATES', 'templates/html/');
+define ('SUBTEMPLATES', 'templates/html/sub_templates/');
 define ('MYSQL', 'models/MYSQL.php');
 define ('SALT', 'bookshop');
 
