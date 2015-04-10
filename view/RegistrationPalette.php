b28162c14d0f36042d15c1bc483fe57dc6c32c43
SimonJivotnoe <nk271085mam@gmail.com>
2015-04-10 10:50:13 +0300

    Update RegistrationPalette.php

--- view/RegistrationPalette.php
+++ view/RegistrationPalette.php
@@ -1,13 +1,22 @@
 <?php
 
 
+/**
+ * Class RegistrationPalette
+ */
 class RegistrationPalette {
 
 
+    /**
+     *
+     */
     public function __construct() {
        
     }
 
+    /**
+     * @return array
+     */
     public function getArr(){
          return DataContModel::getInstance()->getData();
     }
