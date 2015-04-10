b63c098be808a9ac1c93064f5bb64d44919f34ff
SimonJivotnoe <nk271085mam@gmail.com>
2015-04-10 10:39:26 +0300

    Update Ajaxauthorsctrl.php

--- controllers/Ajaxauthorsctrl.php
+++ controllers/Ajaxauthorsctrl.php
@@ -1,7 +1,7 @@
 <?php
 
 
-class Ajaxauthorsctrl {
+/*class Ajaxauthorsctrl {
 
     public function __construct(){
         $pdo = DataBaseModel::connect();
@@ -13,6 +13,21 @@ class Ajaxauthorsctrl {
 			and a.id = ".$_GET['author_id']."
 			GROUP BY b.name")
             ->exec();
+        DataContModel::getInstance()->setData($listOfAuthorBooks);
+    }
+
+} */
+/**
+ * Class Ajaxauthorsctrl
+ */
+class Ajaxauthorsctrl {
+
+    /**
+     *
+     */
+    public function __construct(){
+        $objModel = new AgentDBModel();
+        $listOfAuthorBooks = $objModel->getListOfAuthorBooks($_GET['author_id']);
         DataContModel::getInstance()->setData($listOfAuthorBooks);
     }
 
