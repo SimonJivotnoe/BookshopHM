14142bc2f0c90fd7ba73b184e00d42e1c799bee6
SimonJivotnoe <nk271085mam@gmail.com>
2015-04-10 10:50:31 +0300

    Update ThankyouPalette.php

--- view/ThankyouPalette.php
+++ view/ThankyouPalette.php
@@ -1,13 +1,23 @@
 <?php
 
 
+/**
+ * Class ThankyouPalette
+ */
 class ThankyouPalette {
     private $repArr = array();
+
+    /**
+     *
+     */
     public function __construct()
     {
 
     }
 
+    /**
+     * @return array
+     */
     public function getArr()
     {
         $obj = DataContModel::getInstance();
